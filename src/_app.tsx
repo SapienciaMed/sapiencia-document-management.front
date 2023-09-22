63fbdc16a6d172221eb56c07707d659db5bb9f24
Francisco <gaviria.reyes@gmail.com>
2023-09-22 08:13:48 -0500

    adding routes

--- src/_app.tsx
+++ src/_app.tsx
@@ -2,45 +2,57 @@ import React, { Suspense, lazy, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AppContextProvider } from "./common/contexts/app.context";
 import "./styles/_app.scss";
-import "./styles/_theme-prime.css";
+//import "./styles/_theme-prime.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
 import ModalMessageComponent from "./common/components/modal-message.component";
 import ApplicationProvider from "./application-provider";
 
 import useAppCominicator from "./common/hooks/app-communicator.hook";
 import DocumentsRoutes from "./features/documents/documents-routes";
+import DocumentsReceived from "./features/documents/pages/documents-received";
 
 function App() {
-  const { publish } = useAppCominicator();
-  const HomePage = lazy(() => import("./common/components/home.page"));
+	const { publish } = useAppCominicator();
+	const HomePage = lazy(() => import("./common/components/home.page"));
 
-  // Effect que comunica la aplicacion actual
-  useEffect(() => {
-    localStorage.setItem("currentAplication", process.env.aplicationId);
-    setTimeout(
-      () => publish("currentAplication", process.env.aplicationId),
-      500
-    );
-  }, []);
+	// Effect que comunica la aplicacion actual
+	useEffect(() => {
+		localStorage.setItem("currentAplication", process.env.aplicationId);
+		setTimeout(
+			() => publish("currentAplication", process.env.aplicationId),
+			500
+		);
+	}, []);
 
-  return (
-    <AppContextProvider>
-      <ModalMessageComponent />
-      <ApplicationProvider>
-        <Router>
-          <Suspense fallback={<p>Loading...</p>}>
-            <Routes>
-              <Route path={"/gestion-documental/"} element={<HomePage />} />;
-              <Route
-                path={"/gestion-documental/documentos/*"}
-                element={<DocumentsRoutes />}
-              />
-            </Routes>
-          </Suspense>
-        </Router>
-      </ApplicationProvider>
-    </AppContextProvider>
-  );
+	return (
+		<AppContextProvider>
+			<ModalMessageComponent />
+			<ApplicationProvider>
+				<Router>
+					<Suspense fallback={<p>Loading...</p>}>
+						<Routes>
+							<Route
+								path={"/gestion-documental/"}
+								element={<HomePage />}
+							/>
+							;
+							<Route
+								path={"/gestion-documental/documentos/*"}
+								element={<DocumentsRoutes />}
+							/>
+							<Route
+								path={"/gestion-documental/radicado"}
+								element={<DocumentsReceived />}
+							/>
+						</Routes>
+					</Suspense>
+				</Router>
+			</ApplicationProvider>
+		</AppContextProvider>
+	);
 }
 
 export default React.memo(App);
