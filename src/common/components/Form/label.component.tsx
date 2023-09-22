a494c2da107ce929cfd2863ae4b2f332894f89a2
Francisco <gaviria.reyes@gmail.com>
2023-09-22 07:55:03 -0500

    adding common components

--- src/common/components/Form/label.component.tsx
+++ src/common/components/Form/label.component.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 interface ILabelProps {
-  value: string;
-  className?: string;
-  htmlFor?: string;
+	value: string;
+	className?: string;
+	htmlFor?: string;
 }
 
 export function LabelComponent({
-  value,
-  className = "labelcss",
-  htmlFor,
+	value,
+	className = "labelcss",
+	htmlFor,
 }: ILabelProps): React.JSX.Element {
-  return (
-    <label htmlFor={htmlFor} className={className}>
-      {value}
-    </label>
-  );
+	return (
+		<label htmlFor={htmlFor} className={className}>
+			{value}
+		</label>
+	);
 }
